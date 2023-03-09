fd78b4529ec3f88d40ce6b059b8b84061ff5ca6e
Nixonsama <109393166+Nixonsama@users.noreply.github.com>
2023-03-08 19:43:25 -0500

    Delete modules directory

--- modules/ec2/ansible.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-sudo adduser ansible
-echo "ansible:ansible" | chpasswd
-echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
-sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
-sudo service sshd restart
-sudo su - ansible
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt install ansible -y
